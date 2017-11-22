0b76373217dd4c60542a8c1a42c90676397c5f57
Justin Mancinelli <justin.mancinelli@gmail.com>
2017-11-21 21:35:36 -0500

    Complete fourth tutorial
    https://coderjourney.com/tutorials/react-state-vs-props/
    
    Update ChordEditor component to use props instead of state
    
    Update App component to pass props to ChordEditor

--- chords/src/components/ChordEditor.test.js
+++ chords/src/components/ChordEditor.test.js
@@ -4,34 +4,17 @@ import ChordEditor from './ChordEditor';
 
 describe('<ChordEditor />', () => {
   it('renders an editor area', () => {
-    const editor = shallow(<ChordEditor />);
+    const editor = shallow(<ChordEditor song={{chordpro: ""}}/>);
     expect(editor.find('textarea').length).toEqual(1);
   });
 
   it('renders an output area', () => {
-    const editor = shallow(<ChordEditor />);
+    const editor = shallow(<ChordEditor song={{chordpro: ""}}/>);
     expect(editor.find('div.chord-output').length).toEqual(1);
   });
 
-  it('generates the chord chart output', () => {
-    const editor = shallow(<ChordEditor />);
-    const expected = 
-      '<table>' +
-        '<tr>' +
-          '<td class="chord"></td>' +
-        '</tr>' +
-        '<tr>' +
-          '<td class="lyrics">Type some lyrics here&nbsp;</td>' +
-        '</tr>' +
-      '</table>';
-
-    const result = editor.find('div.chord-output').html();
-
-    expect(result.indexOf(expected) > -1).toEqual(true);
-  });
-
-  it('regenerates the chord chart output based on the state', () => {
-    const editor = shallow(<ChordEditor />);
+  it('generates the chord chart output based on the prop', () => {
+    const editor = shallow(<ChordEditor song={{chordpro: '[B]New [Am]Lyrics'}}/>);
     const expected =
       '<table>' +
         '<tr>' +
@@ -43,10 +26,22 @@ describe('<ChordEditor />', () => {
           '<td class="lyrics">Lyrics&nbsp;</td>' +
         '</tr>' +
       '</table>';
-    editor.setState({ value: "[B]New [Am]Lyrics"});
 
     const result = editor.find('div.chord-output').html();
 
     expect(result.indexOf(expected) > -1).toEqual(true);
+  });
+
+  it('calls updateSong when the textarea changes', () => {
+    var theSong
+    const update = (song) => {
+      theSong = song;
+    };
+
+    const editor = shallow(<ChordEditor song={{chordpro: '[B]New [Am]Lyrics'}} updateSong={update}/>);
+
+    editor.find('textarea').simulate('change', {target: {value: '[B]New [Am]Lyrics'}});
+
+    expect(theSong).toEqual({chordpro: '[B]New [Am]Lyrics'});
   });
 });
