5e77f20ccfc33e84b23738d03d858d4d121aca1e
Justin Mancinelli <justin.mancinelli@gmail.com>
2017-12-26 21:16:19 -0500

    Completed seventh tutorial
    https://coderjourney.com/tutorials/how-to-add-blueprintjs-to-a-react-app/
    
    Add style, both inline and from blueprintjs

--- chords/src/App.js
+++ chords/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { base } from './base';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ChordEditor from './components/ChordEditor';
+import SongList from './components/SongList';
 
 class App extends Component {
   constructor() {
@@ -48,37 +49,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="wrapper">
-        <Header />
-        <div className="workspace">
-          <BrowserRouter>
-            <div className="main-content">
-              <Route exact path="/songs" render={(props) => {
-                const songIds = Object.keys(this.state.songs)
-                return (
-                  <ul>
-                    {songIds.map((id) => {
-                      return (
-                        <li key={id}>
-                          <Link to={`/songs/${id}`}>Song {id}</Link>
-                        </li>
-                      )
-                    })}
-                  </ul>
-                )
-              }} />
-              <Route path="/songs/:songId" render={(props) => {
-                const song = this.state.songs[props.match.params.songId];
-                return (
-                  song
-                  ? <ChordEditor song={song} updateSong={this.updateSong} />
-                  : <h1>Song not found</h1>
-                )
-              }} />
+      <div style={{maxWidth: "1160px", margin: "0 auto"}}>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <div className="main-content" style={{padding: "1em"}}>
+              <div className="workspace">
+                <Route exact path="/songs" render={(props) => {
+                  return <SongList songs={this.state.songs} />
+                }} />
+                <Route path="/songs/:songId" render={(props) => {
+                  const song = this.state.songs[props.match.params.songId];
+                  return (
+                    song
+                    ? <ChordEditor song={song} updateSong={this.updateSong} />
+                    : <h1>Song not found</h1>
+                  )
+                }} />
+              </div>
             </div>
-          </BrowserRouter>
-        </div>
-        <Footer />
+            <Footer />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
